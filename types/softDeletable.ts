b4fbad6055c9b7ea1ea8f694cef9e8d79f8f8dc4
John Ciolfi <johnc2744@gmail.com>
2022-02-10 12:40:51 -0500

    englizh

--- types/softDeletable.ts
+++ types/softDeletable.ts
@@ -1,6 +1,6 @@
 import { IBaseType } from './baseType';
 
-// Base type for all schema that will be soft-deletable for data recovery
+// Base type for some schema that will be soft-deletable for data recovery
 export interface ISoftDeletable extends IBaseType {
   deleted: boolean;
   date_deleted: Date;
